da7c5ff4e03fc1c4f5d24b015b7643f6a3645ff5
Antoine GIRARD <sapk@sapk.fr>
2016-12-02 21:26:56 +0100

    ...

--- gvfs/drivers/ftp_test.go
+++ gvfs/drivers/ftp_test.go
@@ -1,48 +1,63 @@
 package drivers
 
 import (
+	"net/url"
 	"testing"
 )
 
 func TestSimpleIPURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://10.0.0.1"}
-	if tmp.mountpoint() != "ftp:host=10.0.0.1" {
-		t.Error("Expected ftp:host=10.0.0.1, got ", v)
+	u, _ := url.Parse("ftp://10.0.0.1")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=10.0.0.1" {
+		t.Error("Expected ftp:host=10.0.0.1, got ", m, err)
 	}
 }
 func TestHostURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://host"}
-	if tmp.mountpoint() != "ftp:host=host" {
-		t.Error("Expected ftp:host=host, got ", v)
+	u, _ := url.Parse("ftp://host")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host" {
+		t.Error("Expected ftp:host=host, got ", m, err)
 	}
 }
 func TestUserInURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://user@host"}
-	if tmp.mountpoint() != "ftp:host=host,user=user" {
-		t.Error("Expected ftp:host=host,user=user, got ", v)
+	u, _ := url.Parse("ftp://user@host")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host,user=user" {
+		t.Error("Expected ftp:host=host,user=user, got ", m, err)
 	}
 }
 func TestPortInURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://host:42"}
-	if tmp.mountpoint() != "ftp:host=host,port=42" {
-		t.Error("Expected ftp:host=host,port=42, got ", v)
+	u, _ := url.Parse("ftp://host:42")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host,port=42" {
+		t.Error("Expected ftp:host=host,port=42, got ", m, err)
 	}
 }
 func TestDefaultPortInURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://host:21"}
-	if tmp.mountpoint() != "ftp:host=host" {
-		t.Error("Expected ftp:host=host, got ", v)
+	u, _ := url.Parse("ftp://host:21")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host" {
+		t.Error("Expected ftp:host=host, got ", m, err)
 	}
 }
 func TestFullURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://user@host:42"}
-	if tmp.mountpoint() != "ftp:host=host,port=42,user=sapk" {
-		t.Error("Expected ftp:host=host,port=42,user=user, got ", v)
+	u, _ := url.Parse("ftp://user@host:42")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host,port=42,user=user" {
+		t.Error("Expected ftp:host=host,port=42,user=user, got ", m, err)
 	}
 }
 func TestFullDefaultPortURL(t *testing.T) {
-	tmp := FTPVolumeDriver{url: "ftp://user@host:21"}
-	if tmp.mountpoint() != "ftp:host=host,user=sapk" {
-		t.Error("Expected ftp:host=host,user=user, got ", v)
+	u, _ := url.Parse("ftp://user@host:21")
+	tmp := FTPVolumeDriver{url: u}
+	m, err := tmp.mountpoint()
+	if err != nil || m != "ftp:host=host,user=user" {
+		t.Error("Expected ftp:host=host,user=user, got ", m, err)
 	}
 }
